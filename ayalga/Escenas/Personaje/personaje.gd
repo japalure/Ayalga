1e31b919800afc270ab76f884aa18efff001c5b0
VictorCollants <victorcollantesrodriguez@gmail.com>
2026-02-23 22:50:19 +0100

    Culetazo versión 1

--- ayalga/Escenas/Personaje/personaje.gd
+++ ayalga/Escenas/Personaje/personaje.gd
@@ -2,15 +2,25 @@ extends CharacterBody2D
 
 @export var animacion: AnimatedSprite2D
 @export var area_daño: Area2D
+@export var raycast_suelo: RayCast2D
 
+#movimiento
 const _velocidad:float = 300.0
 const _velocidad_salto:float = -400.0
+var _tiempo_en_aire: float = 0.0;
+#culetazo
+var _altura_inicial_culetazo: float = 0.0
+const _distancia_culetazo:float = 200.0
+const _velocidad_culetazo:float = 300.0
+#estados
 var _muerto: bool = false
+var _golpeando: bool = false
 
 func _ready() -> void:
 	area_daño.body_entered.connect(_on_area_daño_body_entered)
 	
-func _physics_process(delta: float) -> void:
+func _physics_process(delta: float) -> void:	
+	
 	if _muerto:
 		return
 	# gravedad
@@ -18,28 +28,87 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	salto()
+	culetazo()
 	movimiento_lateral()
 	move_and_slide()
 	cambio_animacion()
 
-# Manejo de salto
-func salto() -> void:
-	if Input.is_action_just_pressed("saltar") and is_on_floor():
-		velocity.y = _velocidad_salto
 
 # Maneja movimiento flechas izquierda y derecha
 func movimiento_lateral() -> void:
-	if Input.is_action_pressed("derecha"):
-		velocity.x = _velocidad
-	elif Input.is_action_pressed("izquierda"):
-		velocity.x = -_velocidad
+	if !_golpeando:
+		if Input.is_action_pressed("derecha"):
+			velocity.x = _velocidad
+		elif Input.is_action_pressed("izquierda"):
+			velocity.x = -_velocidad
+		else:
+			velocity.x = move_toward(velocity.x, 0, _velocidad)
+
+
+# Manejo de salto
+func salto() -> void:
+	
+	if Input.is_action_just_pressed("saltar") and is_on_floor():
+		velocity.y = _velocidad_salto
+		
+		
+
+#Manejo culetazo
+func culetazo() -> void:
+	if(Input.is_action_just_pressed("culetazo") && !is_on_floor() && !_golpeando):
+		iniciar_culetazo()		
+		
+	if _golpeando:
+		procesar_culetazo()
+
+func iniciar_culetazo() ->void:
+	_golpeando = true
+	_altura_inicial_culetazo = position.y
+	
+
+func procesar_culetazo()->void:
+	velocity.x = 0
+	velocity.y = _velocidad_culetazo
+	
+	var distancia_recorrida = position.y - _altura_inicial_culetazo
+	
+	if distancia_recorrida >= _distancia_culetazo:
+		terminar_culetazo()
+	elif detectar_contacto_culetazo():
+		procesar_contacto_culetazo()
+	
+func terminar_culetazo()-> void:
+	_golpeando = false
+	
+	
+	
+func detectar_contacto_culetazo()->bool:
+	if get_slide_collision_count() > 0:
+		return true
 	else:
-		velocity.x = move_toward(velocity.x, 0, _velocidad)
+		return false
+	
+	
+func procesar_contacto_culetazo() ->void:
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var objeto = collision.get_collider()
+		print("Golpee: ", objeto.name)
+		
+		if objeto.name == 'Enemigo':
+			if collision.get_normal() == Vector2.UP:
+				print("Golpee: ", objeto.name)
+				velocity.y = -300 # rebote
+				terminar_culetazo()
+		else:
+			terminar_culetazo()
 
 # Cambia la animación del personaje dependiendo de su estaso
 func cambio_animacion() -> void:
-	if !is_on_floor():
+	if !is_on_floor() && !_golpeando:
 		animacion.play("saltar")
+	elif !is_on_floor() && _golpeando:
+		animacion.play("culetazo")
 	elif velocity.x != 0:
 		animacion.play("correr")
 	else:
